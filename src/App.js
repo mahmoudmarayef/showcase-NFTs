4f98e0d80563bb018f739b444444e4fc562373b3
Mahmoud <mahmoudmarayef@gmail.com>
2022-05-01 00:30:28 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { Download, Features, SeactionWrapper } from "./components";
+import assets from './assets';
+import styles from "./styles/Global";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <SeactionWrapper
+        title="Your own store of Nifty NFTs. Start Selling & Growing."
+        description="Buy, store, collect NFTs, exchange & earn Crypto. Join +25M people using ProNef Marketplace."
+        showBtn
+        mockupImg={assets.homeHero}
+        banner="banner"
+      />
+      <SeactionWrapper
+        title="Smart User Interface Marketplace"
+        description="Exprience a buttery UI of ProNef NFT Marketplace."
+        mockupImg={assets.homeCards}
+        reverse
+      />
+      <Features />
+      <SeactionWrapper
+        title="Deployment"
+        description="ProNef is build using Expo which runs natively on all user's devices"
+        mockupImg={assets.feature}
+        reverse
+      />
+      <SeactionWrapper
+        title="Creative way to showcase the store"
+        description="The app contains two screens. The first screen lists all NFTs While the second one shows the details of a specific NFT."
+        mockupImg={assets.mockup}
+        banner="banner02"
+      />
+      <Download />
+
+      <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>Made with love by {" "} <span className="bold">mmarayef</span></p>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Features from "./Features";
+import Download from "./Download";
+import SeactionWrapper from "./SeactionWrapper";
+
+export { Features, Download, SeactionWrapper };
